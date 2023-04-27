7ea1964d3534d17b85c5e62b6d988f040c0a9244
kedar nath singh <nathkd@gmail.com>
2023-04-28 00:37:44 +0530

    jsx basics elements components and other way pass javascript inside jsx code.

--- app.js
+++ app.js
@@ -1,33 +1,60 @@
 import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-{/* <div id="parent">
-<div id="child">
-<h1>This is a H1 tag.</h1>
-<h2>This is a H2 tag.</h2>
-</div>
-<div id="child2">
-<h1>This is a H1 tag.</h1>
-<h2>This is a H2 tag.</h2>
-</div>
-</div> */}
+header = React.createElement("h1", {}, "This is a H1 tag");
+console.log(header);
+// jsx- html like or xml like syntax 
+// For multiline code () braces is required
+// React Element
+const element = <span>This is an element.</span>
+const jsxheading =(<h1 id="heading">
+    This is a heading.
+    {element}
+    </h1>);
+console.log(jsxheading);
 
-const parent = React.createElement("div", {id: "parent"},[
-    React.createElement("div", {id: "child"},[
-        React.createElement("h1", {}, "This is a H1 tag"),
-        React.createElement("h2", {}, "This is a H2 tag")
-    ]),
-    React.createElement("div", {id: "child2"},[
-        React.createElement("h1", {}, "This is a H1 tag"),
-        React.createElement("h2", {}, "This is a H2 tag")
-    ])
-]
+// Component
+// Class Based component
+// Functional component
+// React Component
+const HeaderComponent = () => {
+    return(<div>
+        This is a react functional component. 
+    </div>)
+}
+// Component Composition
+const HeadingComponent = () => (
+    <div id="tag1">
+        <h1>This is a React code</h1>
+        {jsxheading}
+        {HeaderComponent()}
+        <HeaderComponent />
+
+    </div>
 );
 
+// React Fragnent  - behaves like an empty tag
+
+const HeadingComponent2 = () => (
+    // <React.Fragment>
+    <>
+    <div id="tag1">
+        <h1>This is a React code</h1>
+        {jsxheading}
+        {HeaderComponent()}
+        <HeaderComponent />
+    </div>
+    <div> Test Node</div>
+    </>
+    // </React.Fragment>
+);
+
+root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent2/>);
 
 
-const heading = React.createElement('h1', {'id': 'test'}, 'Hello World from React 2!');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent); 
+
+
 
 
